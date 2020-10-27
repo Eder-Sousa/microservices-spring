b9e400f107351f5a7b30594414551c04fa048e1d
Eder-Sousa <ederdi395@gmail.com>
2020-10-26 22:20:21 -0300

    Project hr-eureca-server created

---
+++ hr-eureca-server/src/test/java/com/edersousa/hreurecaserver/HrEurecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersousa.hreurecaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurecaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
