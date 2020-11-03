22f9edb95f74ca26a4bcc2a54a5728148b24fe39
Eder-Sousa <ederdi395@gmail.com>
2020-11-02 21:38:54 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/edersousa/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/edersousa/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.edersousa.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.edersousa.hroauth.entities.User;
 import com.edersousa.hroauth.feignclients.UserFeingClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,5 +28,17 @@ public class UserService {
 		
 		logger.info("Email found: " + email);
 		return user;		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeingClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email found: " + username);
+		return user;
 	}
 }
